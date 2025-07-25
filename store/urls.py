b59376ec75a91e7944b25c274e437b7fca265edc
Sajjad124629 <sh8170468@gmail.com>
2025-07-25 13:59:20 +0600

    Api initiligation

---
+++ store/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework_nested import routers
+from . import views
+router = routers.DefaultRouter()
+router.register('products', views.ProductViewSet,basename='products')
+router.register('collections', views.collectionViewSet,basename='collections')
+products_router = routers.NestedDefaultRouter(router,'products',lookup='product')
+products_router.register('reviews', views.ReviewViewSet,basename='product-reviews')
+# URLConf
+urlpatterns = [
+    path('',include(router.urls)),
+    path('',include(products_router.urls)),
+]
---
+++ store/views.py
@@ -0,0 +1,38 @@
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
+from rest_framework.filters import SearchFilter,OrderingFilter
+from django.db.models import Count
+from store.filters import ProductFilter
+from store.models import Collection, OrderItem, Product, Review
+from store.pagination import DefaultPagination
+from store.serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter,OrderingFilter]
+    filterset_class = ProductFilter
+    pagination_class = DefaultPagination
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price','last_update']
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
+            return Response({'error': 'Product   cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return super().destroy(request, *args, **kwargs)
+   
+class collectionViewSet(ModelViewSet):
+    queryset = Collection.objects.annotate(product_count=Count('product')).all()
+    serializer_class = CollectionSerializer
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
+            return Response({'error': 'Collection cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return super().destroy(request, *args, **kwargs)
+  
+class ReviewViewSet(ModelViewSet):
+    serializer_class = ReviewSerializer
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']} 
